2ecd96a4ac4fca43b5c2203b825691a1db14cacc
keikod1 <madragimova@icloud.com>
2022-02-13 14:31:03 +0600

    Initial Commit

---
+++ newsAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsAppTest
+//
+//  Created by my macbook on 13/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
